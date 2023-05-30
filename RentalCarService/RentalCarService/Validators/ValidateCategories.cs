d3d1ba8494d0648f5188224b495950ff8bf76457
JuMaxi <jmaximovski@gmail.com>
2023-05-30 10:21:15 +0100

    Refactoring ValidateCategories

--- RentalCarService/RentalCarService/Validators/ValidateCategories.cs
+++ RentalCarService/RentalCarService/Validators/ValidateCategories.cs
@@ -1,11 +1,18 @@
 ﻿using RentalCarService.Interfaces;
 using RentalCarService.Models;
 using System;
+using System.Data;
 
 namespace RentalCarService.Validators
 {
     public class ValidateCategories : IValidateCategories
     {
+        IAccessDataBase AccessDB;
+        public ValidateCategories(IAccessDataBase Acccess) 
+        { 
+            AccessDB= Acccess;
+        }
+
         public void ValidateCategory(Categories Category)
         {
             if(Category.Code == null
@@ -15,11 +22,27 @@ namespace RentalCarService.Validators
             }
             else
             {
-                if(Category.Description == null
+                ValidateUniqueCodeCategory(Category);
+                ValidateDescriptionCategory(Category);
+            }
+        }
+        private void ValidateUniqueCodeCategory(Categories Category)
+        {
+            string Select = "select * from Categories where Code='" + Category.Code + "'";
+
+            IDataReader Reader = AccessDB.AccessReader(Select); 
+
+            while(Reader.Read())
+            {
+                throw new Exception("The Code Category must be unique. Change the code to continue.");
+            }
+        }
+        private void ValidateDescriptionCategory(Categories Category)
+        {
+            if (Category.Description == null
                     || Category.Description.Length == 0)
-                {
-                    throw new Exception("The Description of the car must be filled to continue.");
-                }
+            {
+                throw new Exception("The Description of the car must be filled to continue.");
             }
         }
     }
