7e54d3278237fab034bca33cf08b2f22e32b3088
JuMaxi <jmaximovski@gmail.com>
2023-08-15 19:02:35 +0100

    Creating_AvailabilityResponse_Model

---
+++ RentalCarService/RentalCarService/Models/Responses/Availability.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RentalCarService.Models.Responses
+{
+    public class Availability
+    {
+        public Categories Category { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public double Estimative { get; set; }
+
+    }
+}
--- RentalCarService/RentalCarService/Services/BookingService.cs
+++ RentalCarService/RentalCarService/Services/BookingService.cs
@@ -163,6 +163,7 @@ namespace RentalCarService.Services
             }
         }
 
+
     }
 }
 
