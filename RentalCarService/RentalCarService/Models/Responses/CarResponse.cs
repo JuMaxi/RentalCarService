1b8b9dfdd3cb33cc41abdb5302b8a30b94695b29
JuMaxi <jmaximovski@gmail.com>
2023-08-24 14:29:33 +0100

    CarResponse

---
+++ RentalCarService/RentalCarService/Models/Responses/CarResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RentalCarService.Models.Responses
+{
+    public class CarResponse
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public DateTime Year { get; set; }
+        public string Transmission { get; set; }
+        public int Doors { get; set; }
+        public int Seats { get; set; }
+        public string AirConditioner { get; set; }
+        public int TrunkSize { get; set; }
+        public string NumberPlate { get; set; }
+        public string Category { get; set; }
+        public string Branch { get; set; }
+    }
+}
