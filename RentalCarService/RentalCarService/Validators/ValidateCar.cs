6d21e88eca9f08c371d8a617bad4b599a2f3486a
JuMaxi <jmaximovski@gmail.com>
2023-08-26 16:48:01 +0100

    ValidateCarsTests

--- RentalCarService/RentalCarService/Validators/ValidateCar.cs
+++ RentalCarService/RentalCarService/Validators/ValidateCar.cs
@@ -9,7 +9,7 @@ namespace RentalCarService.Validators
     {
         public void Validate(Car Car)
         {
-            if (Car.Model.Length == 0 || Car.Model == null)
+            if (Car.Model is null || Car.Model.Length == 0)
             {
                 throw new Exception("The Model Car must be filled to continue and can't be null.");
             }
@@ -18,27 +18,27 @@ namespace RentalCarService.Validators
             {
                 throw new Exception("The Year must less or equal the current year.");
             }
-            if (Car.Transmission.Length == 0 || Car.Transmission == null)
+            if (Car.Transmission is null || Car.Transmission.Length == 0)
             {
                 throw new Exception("The Transmission Car must be filled to continue and can't be null.");
             }
-            if(Car.Doors <= 0)
+            if (Car.Doors <= 0)
             {
                 throw new Exception("The Doors Car must be greater than zero.");
             }
-            if(Car.Seats <= 0)
+            if (Car.Seats <= 0)
             {
                 throw new Exception("The Seats Car must be greater than zero.");
             }
-            if(Car.AirConditioner.Length == 0 || Car.AirConditioner== null)
+            if (Car.AirConditioner is null || Car.AirConditioner.Length == 0)
             {
                 throw new Exception("The AirConditioner must be filled to continue and can't be null");
             }
-            if(Car.TrunkSize <= 0)
+            if (Car.TrunkSize <= 0)
             {
                 throw new Exception("The Trunk Size must be greater than zero.");
             }
-            if(Car.NumberPlate.Length== 0 || Car.NumberPlate == null)
+            if (Car.NumberPlate is null || Car.NumberPlate.Length == 0)
             {
                 throw new Exception("The Number Plate Car must be filled to continue and can't be null.");
             }
---
+++ RentalCarService/RentarlCars.Tests/Validators/ValidateCarsTests.cs
@@ -0,0 +1,314 @@
+﻿using FluentAssertions;
+using RentalCarService.Models;
+using RentalCarService.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentarlCars.Tests.Validators
+{
+    public class ValidateCarsTests
+    {
+        [Fact]
+        public void When_model_is_null_should_throw_new_exception()
+        {
+            Car car = new()
+            {
+                Model = null
+            };
+
+            ValidateCar validatorCar = new();
+
+            validatorCar.Invoking(validator => validator.Validate(car))
+                .Should().Throw<Exception>()
+                .WithMessage("The Model Car must be filled to continue and can't be null.");
+        }
+
+        [Fact]
+        public void When_model_length_is_zero_should_throw_new_exception()
+        {
+            Car car = new()
+            {
+                Model = ""
+            };
+
+            ValidateCar validatorCar = new();
+
+            validatorCar.Invoking(validator => validator.Validate(car))
+                .Should().Throw<Exception>()
+                .WithMessage("The Model Car must be filled to continue and can't be null.");
+        }
+
+        [Fact]
+        public void When_year_is_greater_than_actual_year_should_throw_new_exception()
+        {
+            Car car = new()
+            {
+                Model = "Monza",
+                Year = new DateTime(2024, 01, 01)
+            };
+
+            ValidateCar validatorCar = new();
+            validatorCar.Invoking(validator => validator.Validate(car))
+                .Should().Throw<Exception>()
+                .WithMessage("The Year must less or equal the current year.");
+        }
+
+        [Fact]
+        public void When_transmition_is_null_should_throw_new_Exception()
+        {
+            Car car = new()
+            {
+                Model = "Monza",
+                Year = new DateTime(2023, 01, 01),
+                Transmission = null
+            };
+
+            ValidateCar validatorCar = new();
+
+            validatorCar.Invoking(validator => validator.Validate(car))
+                .Should().Throw<Exception>()
+                .WithMessage("The Transmission Car must be filled to continue and can't be null.");
+        }
+
+        [Fact]
+        public void When_transmition_length_is_zero_should_throw_new_exception()
+        {
+            Car car = new()
+            {
+                Model = "Monza",
+                Year = new DateTime(2023, 01, 01),
+                Transmission = ""
+            };
+
+            ValidateCar validatorCar = new();
+
+            validatorCar.Invoking(validator => validator.Validate(car))
+                .Should().Throw<Exception>()
+                .WithMessage("The Transmission Car must be filled to continue and can't be null.");
+        }
+
+        [Fact]
+        public void When_doors_is_zero_should_throw_new_exception()
+        {
+            Car car = new()
+            {
+                Model = "Monz",
+                Year = new DateTime(2023, 01, 01),
+                Transmission = "Automatic",
+                Doors = 0
+            };
+
+            ValidateCar validatorCar = new();
+
+            validatorCar.Invoking(validator => validator.Validate(car))
+                .Should().Throw<Exception>()
+                .WithMessage("The Doors Car must be greater than zero.");
+        }
+
+        [Fact]
+        public void When_doors_is_less_than_zero_should_throw_new_exception()
+        {
+            Car car = new()
+            {
+                Model = "Monza",
+                Year = new DateTime(2023, 01, 01),
+                Transmission = "Automatic",
+                Doors = -1
+            };
+
+            ValidateCar validatorCar = new();
+
+            validatorCar.Invoking(validator => validator.Validate(car))
+                .Should().Throw<Exception>()
+                .WithMessage("The Doors Car must be greater than zero.");
+        }
+
+        [Fact]
+        public void When_seats_is_zero_should_throw_new_exception()
+        {
+            Car car = new()
+            {
+                Model = "Monza",
+                Year = new DateTime(2023, 01, 01),
+                Transmission = "Automatic",
+                Doors = 5,
+                Seats = 0
+            };
+
+            ValidateCar validatorCar = new();
+
+            validatorCar.Invoking(validator => validator.Validate(car))
+                .Should().Throw<Exception>()
+                .WithMessage("The Seats Car must be greater than zero.");
+        }
+
+        [Fact]
+        public void When_seats_is_less_than_zero_should_throw_new_exception()
+        {
+            Car car = new()
+            {
+                Model = "Monza",
+                Year = new DateTime(2023, 01, 01),
+                Transmission = "Automatic",
+                Doors = 5,
+                Seats = -1
+            };
+
+            ValidateCar validatorCar = new();
+
+            validatorCar.Invoking(validator => validator.Validate(car))
+                .Should().Throw<Exception>()
+                .WithMessage("The Seats Car must be greater than zero.");
+        }
+
+        [Fact]
+        public void When_airconditioner_is_null_should_throw_new_exception()
+        {
+            Car car = new()
+            {
+                Model = "Monza",
+                Year = new DateTime(2023, 01, 01),
+                Transmission = "Automatic",
+                Doors = 5,
+                Seats = 5,
+                AirConditioner = null
+            };
+
+            ValidateCar validatorCar = new();
+
+            validatorCar.Invoking(validator => validator.Validate(car))
+                .Should().Throw<Exception>()
+                .WithMessage("The AirConditioner must be filled to continue and can't be null");
+        }
+
+        [Fact]
+        public void When_airconditioner_lenght_is_zero_should_throw_new_exception()
+        {
+            Car car = new()
+            {
+                Model = "Monza",
+                Year = new DateTime(2023, 01, 01),
+                Transmission = "Automatic",
+                Doors = 5,
+                Seats = 5,
+                AirConditioner = ""
+            };
+
+            ValidateCar validatorCar = new();
+
+            validatorCar.Invoking(validator => validator.Validate(car))
+                .Should().Throw<Exception>()
+                .WithMessage("The AirConditioner must be filled to continue and can't be null");
+        }
+
+        [Fact]
+        public void When_trunksize_is_zero_should_throw_new_exception()
+        {
+            Car car = new()
+            {
+                Model = "Monza",
+                Year = new DateTime(2023, 01, 01),
+                Transmission = "Automatic",
+                Doors = 5,
+                Seats = 5,
+                AirConditioner = "S",
+                TrunkSize = 0
+            };
+
+            ValidateCar validatorCar = new();
+
+            validatorCar.Invoking(validator => validator.Validate(car))
+                .Should().Throw<Exception>()
+                .WithMessage("The Trunk Size must be greater than zero.");
+        }
+
+        [Fact]
+        public void When_trunksize_is_less_than_zero_should_throw_new_exception()
+        {
+            Car car = new()
+            {
+                Model = "Monza",
+                Year = new DateTime(2023, 01, 01),
+                Transmission = "Automatic",
+                Doors = 5,
+                Seats = 5,
+                AirConditioner = "S",
+                TrunkSize = -1
+            };
+
+            ValidateCar validatorCar = new();
+
+            validatorCar.Invoking(validator => validator.Validate(car))
+                .Should().Throw<Exception>()
+                .WithMessage("The Trunk Size must be greater than zero.");
+        }
+
+        [Fact]
+        public void When_numberplate_is_null_shoudl_throw_new_exception()
+        {
+            Car car = new()
+            {
+                Model = "Monza",
+                Year = new DateTime(2023, 01, 01),
+                Transmission = "Automatic",
+                Doors = 5,
+                Seats = 5,
+                AirConditioner = "S",
+                TrunkSize = 4,
+                NumberPlate = null
+            };
+
+            ValidateCar validatorCar = new();
+
+            validatorCar.Invoking(validator => validator.Validate(car))
+                .Should().Throw<Exception>()
+                .WithMessage("The Number Plate Car must be filled to continue and can't be null.");
+        }
+
+        [Fact]
+        public void When_numberplate_length_is_zero_should_throw_new_exception()
+        {
+            Car car = new()
+            {
+                Model = "Monza",
+                Year = new DateTime(2023, 01, 01),
+                Transmission = "Automatic",
+                Doors = 5,
+                Seats = 5,
+                AirConditioner = "S",
+                TrunkSize = 4,
+                NumberPlate = ""
+            };
+
+            ValidateCar validatorCar = new();
+
+            validatorCar.Invoking(validator => validator.Validate(car))
+                .Should().Throw<Exception>()
+                .WithMessage("The Number Plate Car must be filled to continue and can't be null.");
+        }
+
+        [Fact]
+        public void When_the_validate_car_doesnot_have_exception()
+        {
+            Car car = new()
+            {
+                Model = "Monza",
+                Year = new DateTime(2023, 01, 01),
+                Transmission = "Automatic",
+                Doors = 5,
+                Seats = 5,
+                AirConditioner = "S",
+                TrunkSize = 4,
+                NumberPlate = "KIJ 4L52"
+            };
+
+            ValidateCar validatorCar = new();
+
+            validatorCar.Invoking(validator => validator.Validate(car))
+                .Should().NotThrow<Exception>();
+        }
+    }
+}
