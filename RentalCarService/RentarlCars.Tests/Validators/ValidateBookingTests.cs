389fbf3b1d3160fa714100c0ff2ff771056ae431
JuMaxi <jmaximovski@gmail.com>
2023-08-30 19:37:40 +0100

    ValidateBookingTests

--- RentalCarService/RentarlCars.Tests/Validators/ValidateBookingTests.cs
+++ RentalCarService/RentarlCars.Tests/Validators/ValidateBookingTests.cs
@@ -37,7 +37,7 @@ namespace RentarlCars.Tests.Validators
 
             var availabilityService = Substitute.For<IAvailabilityService>();
             availabilityService.ExistsAvailabilityForBooking(Arg.Any<Booking>(), Arg.Any<List<Booking>>(), Arg.Any<int>()).Returns(false);
-            
+
             ValidateBooking validatorBooking = new(dbAccessBookingFake, availabilityService, dbAccessCarFake, null, null);
 
             validatorBooking.Invoking(validator => validator.Validate(newBooking))
@@ -228,6 +228,361 @@ namespace RentarlCars.Tests.Validators
             validatorBooking.Invoking(validator => validator.Validate(newBooking))
                 .Should().Throw<Exception>()
                 .WithMessage("The Branch to get the car must be chosen to continue.");
+        }
+
+        [Fact]
+        public void When_branchget_hour_is_not_open_should_throw_new_exception()
+        {
+            Booking newBooking = new()
+            {
+                Category = new()
+                {
+                    Id = 1004,
+                },
+                StartDay = new DateTime(2023, 09, 04, 08, 00, 00),
+                ReturnDay = new DateTime(2023, 09, 05, 17, 00, 00),
+                User = new()
+                {
+                    Id = 1
+                },
+                BranchGet = new()
+                {
+                    Id = 1,
+                    OpeningHours = new()
+                    {
+                        new()
+                        {
+                            DayOfWeek = new DateTime(2023, 09, 04).DayOfWeek,
+                            Opens = new TimeOnly(09, 00, 00)
+                        }
+                    }
+                }
+            };
+
+            var dbAccessBookingFake = Substitute.For<IBookingDBAccess>();
+            dbAccessBookingFake.GetListBookingByCategoryIdAndDatesStartReturn(newBooking).ReturnsNull();
+
+            var availabilityService = Substitute.For<IAvailabilityService>();
+            availabilityService.ExistsAvailabilityForBooking(Arg.Any<Booking>(), Arg.Any<List<Booking>>(), Arg.Any<int>()).Returns(true);
+
+            Car car = new();
+            var dbAccessCarFake = Substitute.For<ICarDBAccess>();
+            dbAccessCarFake.GetCarByCategoryId(newBooking.Category.Id).Returns(car);
+
+            var dbAccessUserFake = Substitute.For<IUserDBAccess>();
+            dbAccessUserFake.GetUserById(newBooking.User.Id).Returns(newBooking.User);
+
+            var dbAccessBranchFake = Substitute.For<IBranchesDBAccess>();
+            dbAccessBranchFake.GetBranchById(newBooking.BranchGet.Id).Returns(newBooking.BranchGet);
+
+            ValidateBooking validatorBooking = new(dbAccessBookingFake, availabilityService, dbAccessCarFake, dbAccessBranchFake, dbAccessUserFake);
+
+            validatorBooking.Invoking(validator => validator.Validate(newBooking))
+                .Should().Throw<Exception>()
+                .WithMessage("This branch opens at " + newBooking.BranchGet.OpeningHours[0].Opens + " you can't get the car before this time.");
+        }
+
+        [Fact]
+        public void When_branchget_dayofweek_is_not_open_should_throw_new_exception()
+        {
+            Booking newBooking = new()
+            {
+                Category = new()
+                {
+                    Id = 1004,
+                },
+                StartDay = new DateTime(2023, 09, 04, 08, 00, 00),
+                ReturnDay = new DateTime(2023, 09, 05, 17, 00, 00),
+                User = new()
+                {
+                    Id = 1
+                },
+                BranchGet = new()
+                {
+                    Id = 1,
+                    OpeningHours = new()
+                    {
+                        new()
+                        {
+                            DayOfWeek = new DateTime(2023, 10, 04).DayOfWeek,
+                            Opens = new TimeOnly(09, 00, 00)
+                        }
+                    }
+                }
+            };
+
+            var dbAccessBookingFake = Substitute.For<IBookingDBAccess>();
+            dbAccessBookingFake.GetListBookingByCategoryIdAndDatesStartReturn(newBooking).ReturnsNull();
+
+            var availabilityService = Substitute.For<IAvailabilityService>();
+            availabilityService.ExistsAvailabilityForBooking(Arg.Any<Booking>(), Arg.Any<List<Booking>>(), Arg.Any<int>()).Returns(true);
+
+            Car car = new();
+            var dbAccessCarFake = Substitute.For<ICarDBAccess>();
+            dbAccessCarFake.GetCarByCategoryId(newBooking.Category.Id).Returns(car);
+
+            var dbAccessUserFake = Substitute.For<IUserDBAccess>();
+            dbAccessUserFake.GetUserById(newBooking.User.Id).Returns(newBooking.User);
+
+            var dbAccessBranchesFake = Substitute.For<IBranchesDBAccess>();
+            dbAccessBranchesFake.GetBranchById(newBooking.BranchGet.Id).Returns(newBooking.BranchGet);
+
+            ValidateBooking validatorBooking = new(dbAccessBookingFake, availabilityService, dbAccessCarFake, dbAccessBranchesFake, dbAccessUserFake);
+
+            validatorBooking.Invoking(validator => validator.Validate(newBooking))
+                .Should().Throw<Exception>()
+                .WithMessage("This branch is not open on day " + newBooking.StartDay.DayOfWeek + ". Please select a day that this branch is open.");
+        }
+
+        [Fact]
+        public void When_branchreturn_is_zero_should_throw_new_exception()
+        {
+            Booking newBooking = new()
+            {
+                Category = new()
+                {
+                    Id = 1004,
+                },
+                StartDay = new DateTime(2023, 09, 04, 08, 00, 00),
+                ReturnDay = new DateTime(2023, 09, 05, 17, 00, 00),
+                User = new()
+                {
+                    Id = 1
+                },
+                BranchGet = new()
+                {
+                    Id = 1,
+                    OpeningHours = new()
+                    {
+                        new()
+                        {
+                            DayOfWeek = new DateTime(2023, 09, 04).DayOfWeek,
+                            Opens = new TimeOnly(07, 00, 00),
+                        }
+                    }
+                },
+                BranchReturn= new()
+                {
+                    Id = 0
+                }
+            };
+
+            var dbAccessBookingFake = Substitute.For<IBookingDBAccess>();
+            dbAccessBookingFake.GetListBookingByCategoryIdAndDatesStartReturn(newBooking).ReturnsNull();
+
+            var availabilityService = Substitute.For<IAvailabilityService>();
+            availabilityService.ExistsAvailabilityForBooking(Arg.Any<Booking>(), Arg.Any<List<Booking>>(), Arg.Any<int>()).Returns(true);
+
+            Car car = new();
+            var dbAccessCarFake = Substitute.For<ICarDBAccess>();
+            dbAccessCarFake.GetCarByCategoryId(newBooking.Category.Id).Returns(car);
+
+            var dbAccessBranchesFake = Substitute.For<IBranchesDBAccess>();
+            dbAccessBranchesFake.GetBranchById(newBooking.BranchReturn.Id).Returns(newBooking.BranchReturn);
+            dbAccessBranchesFake.GetBranchById(newBooking.BranchGet.Id).Returns(newBooking.BranchGet);
+
+            var dbAccessUserFake = Substitute.For<IUserDBAccess>();
+            dbAccessUserFake.GetUserById(newBooking.User.Id).Returns(newBooking.User);
+
+            ValidateBooking validatorBooking = new(dbAccessBookingFake, availabilityService, dbAccessCarFake, dbAccessBranchesFake, dbAccessUserFake);
+
+            validatorBooking.Invoking(validator => validator.Validate(newBooking))
+                .Should().Throw<Exception>()
+                .WithMessage("The Branch to return the car must be chosen to continue.");
+        }
+
+        [Fact]
+        public void When_branchreturn_hour_is_not_open_should_throw_new_exception()
+        {
+            Booking newBooking = new()
+            {
+                Category = new()
+                {
+                    Id = 1004,
+                },
+                StartDay = new DateTime(2023, 09, 04, 08, 00, 00),
+                ReturnDay = new DateTime(2023, 09, 05, 17, 00, 00),
+                User = new()
+                {
+                    Id = 1
+                },
+                BranchGet = new()
+                {
+                    Id = 1,
+                    OpeningHours = new()
+                    {
+                        new()
+                        {
+                            DayOfWeek = new DateTime(2023, 09, 04).DayOfWeek,
+                            Opens = new TimeOnly(07, 00, 00),
+                        }
+                    }
+                },
+                BranchReturn = new()
+                {
+                    Id = 2,
+                    OpeningHours = new()
+                    {
+                        new()
+                        {
+                            DayOfWeek = new DateTime(2023, 09, 05, 17, 00, 00).DayOfWeek,
+                            Closes = new TimeOnly(16, 00, 00)
+                        }
+                    }
+                }
+            };
+
+            var dbAccessBookingFake = Substitute.For<IBookingDBAccess>();
+            dbAccessBookingFake.GetListBookingByCategoryIdAndDatesStartReturn(newBooking).ReturnsNull();
+
+            var availabilityService = Substitute.For<IAvailabilityService>();
+            availabilityService.ExistsAvailabilityForBooking(Arg.Any<Booking>(), Arg.Any<List<Booking>>(), Arg.Any<int>()).Returns(true);
+
+            Car car = new();
+            var dbAccessCarFake = Substitute.For<ICarDBAccess>();
+            dbAccessCarFake.GetCarByCategoryId(Arg.Any<int>()).Returns(car);
+
+            var dbAccessBranchesFake = Substitute.For<IBranchesDBAccess>();
+            dbAccessBranchesFake.GetBranchById(newBooking.BranchGet.Id).Returns(newBooking.BranchGet);
+            dbAccessBranchesFake.GetBranchById(newBooking.BranchReturn.Id).Returns(newBooking.BranchReturn);
+
+            var dbAccessUserFake = Substitute.For<IUserDBAccess>();
+            dbAccessUserFake.GetUserById(newBooking.User.Id).Returns(newBooking.User);
+
+            ValidateBooking validatorBooking = new(dbAccessBookingFake, availabilityService, dbAccessCarFake, dbAccessBranchesFake, dbAccessUserFake);
+
+            validatorBooking.Invoking(validator => validator.Validate(newBooking))
+                .Should().Throw<Exception>()
+                .WithMessage("This branch closes at " + newBooking.BranchReturn.OpeningHours[0].Closes + " you can't return the car after this time.");
+        }
+
+        [Fact]
+        public void When_branchreturn_dayofweek_is_not_open_should_throw_new_exception()
+        {
+            Booking newBooking = new()
+            {
+                Category = new()
+                {
+                    Id = 1004,
+                },
+                StartDay = new DateTime(2023, 09, 04, 08, 00, 00),
+                ReturnDay = new DateTime(2023, 09, 05, 17, 00, 00),
+                User = new()
+                {
+                    Id = 1
+                },
+                BranchGet = new()
+                {
+                    Id = 1,
+                    OpeningHours = new()
+                    {
+                        new()
+                        {
+                            DayOfWeek = new DateTime(2023, 09, 04).DayOfWeek,
+                            Opens = new TimeOnly(07, 00, 00),
+                        }
+                    }
+                },
+                BranchReturn = new()
+                {
+                    Id = 2,
+                    OpeningHours = new()
+                    {
+                        new()
+                        {
+                            DayOfWeek = new DateTime(2023, 09, 04, 17, 00, 00).DayOfWeek,
+                            Closes = new TimeOnly(18, 00, 00)
+                        }
+                    }
+                }
+            };
+
+            var dbAccessBookingFake = Substitute.For<IBookingDBAccess>();
+            dbAccessBookingFake.GetListBookingByCategoryIdAndDatesStartReturn(newBooking).ReturnsNull();
+
+            var availabilityService = Substitute.For<IAvailabilityService>();
+            availabilityService.ExistsAvailabilityForBooking(Arg.Any<Booking>(), Arg.Any<List<Booking>>(), Arg.Any<int>()).Returns(true);
+
+            Car car = new();
+            var dbAccessCarFake = Substitute.For<ICarDBAccess>();
+            dbAccessCarFake.GetCarByCategoryId(newBooking.Category.Id).Returns(car);
+
+            var dbAccessBranchesFake = Substitute.For<IBranchesDBAccess>();
+            dbAccessBranchesFake.GetBranchById(newBooking.BranchGet.Id).Returns(newBooking.BranchGet);
+            dbAccessBranchesFake.GetBranchById(newBooking.BranchReturn.Id).Returns(newBooking.BranchReturn);
+
+            var dbAccessUserFake = Substitute.For<IUserDBAccess>();
+            dbAccessUserFake.GetUserById(newBooking.User.Id).Returns(newBooking.User);
+
+            ValidateBooking validatorBooking = new(dbAccessBookingFake, availabilityService, dbAccessCarFake, dbAccessBranchesFake, dbAccessUserFake);
+
+            validatorBooking.Invoking(validator => validator.Validate(newBooking))
+                .Should().Throw<Exception>()
+                .WithMessage("This branch is not open on day " + newBooking.ReturnDay.DayOfWeek + ". Please select a day that this branch is open.");
+        }
+
+        [Fact]
+        public void When_the_validator_booking_doesnot_have_any_exceptions()
+        {
+            Booking newBooking = new()
+            {
+                Category = new()
+                {
+                    Id = 1004,
+                },
+                StartDay = new DateTime(2023, 09, 04, 08, 00, 00),
+                ReturnDay = new DateTime(2023, 09, 05, 17, 00, 00),
+                User = new()
+                {
+                    Id = 1
+                },
+                BranchGet = new()
+                {
+                    Id = 1,
+                    OpeningHours = new()
+                    {
+                        new()
+                        {
+                            DayOfWeek = new DateTime(2023, 09, 04).DayOfWeek,
+                            Opens = new TimeOnly(07, 00, 00),
+                        }
+                    }
+                },
+                BranchReturn = new()
+                {
+                    Id = 2,
+                    OpeningHours = new()
+                    {
+                        new()
+                        {
+                            DayOfWeek = new DateTime(2023, 09, 05, 17, 00, 00).DayOfWeek,
+                            Closes = new TimeOnly(18, 00, 00)
+                        }
+                    }
+                }
+            };
+
+            var dbAccessBookingFake = Substitute.For<IBookingDBAccess>();
+            dbAccessBookingFake.GetListBookingByCategoryIdAndDatesStartReturn(newBooking).ReturnsNull();
+
+            var availabilityService = Substitute.For<IAvailabilityService>();
+            availabilityService.ExistsAvailabilityForBooking(Arg.Any<Booking>(), Arg.Any<List<Booking>>(), Arg.Any<int>()).Returns(true);
+
+            Car car = new();
+            var dbAccessCarFake = Substitute.For<ICarDBAccess>();
+            dbAccessCarFake.GetCarByCategoryId(newBooking.Category.Id).Returns(car);
+
+            var dbAccessBranchesFake = Substitute.For<IBranchesDBAccess>();
+            dbAccessBranchesFake.GetBranchById(newBooking.BranchGet.Id).Returns(newBooking.BranchGet);
+            dbAccessBranchesFake.GetBranchById(newBooking.BranchReturn.Id).Returns(newBooking.BranchReturn);
+
+            var dbAccessUserFake = Substitute.For<IUserDBAccess>();
+            dbAccessUserFake.GetUserById(newBooking.User.Id).Returns(newBooking.User);
+
+            ValidateBooking validatorBooking = new(dbAccessBookingFake, availabilityService, dbAccessCarFake, dbAccessBranchesFake, dbAccessUserFake);
+
+            validatorBooking.Invoking(validator => validator.Validate(newBooking))
+                .Should().NotThrow<Exception>();
         }
     }
 }
