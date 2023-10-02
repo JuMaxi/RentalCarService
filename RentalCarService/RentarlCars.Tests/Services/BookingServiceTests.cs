a1b0e69dfea4da5717f2ac299c2af2fe16c17908
JuMaxi <jmaximovski@gmail.com>
2023-10-02 17:04:52 +0100

    availabilityCategory_test

--- RentalCarService/RentarlCars.Tests/Services/BookingServiceTests.cs
+++ RentalCarService/RentarlCars.Tests/Services/BookingServiceTests.cs
@@ -85,6 +85,48 @@ namespace RentarlCars.Tests.Services
         }
 
         [Fact]
+        public void When_there_is_a_category_available_should_return_right_estimative_to_pay()
+        {
+            AvailabilityRequest availability = new()
+            {
+                BranchGetCar = 1,
+                StartDay = DateTime.Now,
+                ReturnDay = DateTime.Now.AddDays(2)
+            };
+            List<Categories> listCategories = new()
+            {
+                new()
+                {
+                    Id = 1,
+                    PriceBands = new()
+                    {
+                        new()
+                        {
+                            Price = 10,
+                            MinDays = 1,
+                            MaxDays = 2,
+                        }
+                    }
+                }
+            };
+
+            var availabilityService = Substitute.For<IAvailabilityService>();
+            availabilityService.SaveListAvailableCategories(availability).Returns(listCategories);
+
+            Branchs branch = new();
+            var dbAccessBranchesFake = Substitute.For<IBranchesDBAccess>();
+            dbAccessBranchesFake.GetBranchById(availability.BranchGetCar).Returns(branch);
+
+            var dbAccessCategoriesFake = Substitute.For<ICategoriesDBAccess>();
+            dbAccessCategoriesFake.GetCategoryById(listCategories[0].Id).Returns(listCategories[0]);
+
+            BookingService bookingService = new(null, null, availabilityService, dbAccessCategoriesFake, dbAccessBranchesFake, null, null);
+
+            var availabilityResponse = bookingService.ReturnAvailabilityCategories(availability);
+            availabilityResponse[0].Estimative.Should().Be(20);
+        }
+
+        [Fact]
         public void TesteBlabla()
         {
             var service = new BookingService(null, null, null, null, null, null, null);
